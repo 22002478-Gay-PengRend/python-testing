24419851e947f3357eb763a54067307605e5a8c6
g-pengrend <pengrend@gmail.com>
2023-02-27 11:42:38 +0800

    Revert "Update calc.py"
    This reverts commit 1f7c33bd257bdb731f5acfe06f5e7f3d7a9ee0f3.

--- calc.py
+++ calc.py
@@ -1,13 +1,11 @@
 num1 = float(input("Key in a floating number: "))
 num2 = float(input("Key in another floating number: "))
-sign = input("Key in either + or - or *: ")
+sign = input("Key in either + or -: ")
 
 if sign == "+":
     ans = num1 + num2
 elif sign == "-":
     ans = num1 - num2
-elif sign == "*":
-    ans = num1 * num2
 else:
     print("Invalid input")
 
