34e75d9b3d457503a5d98ae87e1e7c85ac7569f1
g-pengrend <pengrend@gmail.com>
2023-02-27 11:30:15 +0800

    Create calc.py
    created a simple + and - calculator

---
+++ calc.py
@@ -0,0 +1,12 @@
+num1 = float(input("Key in a floating number: "))
+num2 = float(input("Key in another floating number: "))
+sign = input("Key in either + or -: ")
+
+if sign == "+":
+    ans = num1 + num2
+elif sign == "-":
+    ans = num1 - num2
+else:
+    print("Invalid input")
+
+print("Final answer is", ans)
