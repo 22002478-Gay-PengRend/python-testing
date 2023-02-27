45caf0f11df6a6fb1d0ab33493422e99feb1dab6
22002478-Gay-PengRend <99471712+22002478-Gay-PengRend@users.noreply.github.com>
2023-02-27 11:37:59 +0800

    Update calc.py
    adding a multiply

--- calc.py
+++ calc.py
@@ -1,11 +1,13 @@
 num1 = float(input("Key in a floating number: "))
 num2 = float(input("Key in another floating number: "))
-sign = input("Key in either + or -: ")
+sign = input("Key in either + or - or *: ")
 
 if sign == "+":
     ans = num1 + num2
 elif sign == "-":
     ans = num1 - num2
+elif sign == "*":
+    ans = num1 * num2
 else:
     print("Invalid input")
 
