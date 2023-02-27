714ac33140f5d704b88c9322d37d49c38aebff0e
22002478-Gay-PengRend <99471712+22002478-Gay-PengRend@users.noreply.github.com>
2023-02-27 11:48:42 +0800

    Update calc.py
    added a divide

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 num1 = float(input("Key in a floating number: "))
 num2 = float(input("Key in another floating number: "))
-sign = input("Key in either + or - or *: ")
+sign = input("Key in either + or - or * or /: ")
 
 if sign == "+":
     ans = num1 + num2
@@ -8,6 +8,8 @@ elif sign == "-":
     ans = num1 - num2
 elif sign == "*":
     ans = num1 * num2
+elif sing == "/":
+    ans = num1 / num2
 else:
     print("Invalid input")
 
